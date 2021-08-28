9a9870e8bd404c04ed3c7bcef6ddc1e57f17ba64
G.magpie <msk7529@naver.com>
2021-08-29 01:00:57 +0900

    사이드메뉴에 홈라벨 추가, 분리선 추가(firstSection)

--- SideMenuTest/SideMenuTest/Controller/SideMenu/SideMenuViewController.swift
+++ SideMenuTest/SideMenuTest/Controller/SideMenu/SideMenuViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 final class SideMenuViewController: UIViewController {
     // MARK: - Properties
+    private lazy var settingButton: UIButton = {
+        let button: UIButton = .init(frame: .zero)
+        button.setImage(UIImage(named: "settingIcon"), for: .normal)
+        button.addTarget(self, action: #selector(didTapSettingButton), for: .touchUpInside)
+        button.backgroundColor = .clear
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     private let containerView: UIView = {
         let view: UIView = .init(frame: .zero)
         view.backgroundColor = .clear
@@ -23,6 +32,25 @@ final class SideMenuViewController: UIViewController {
         //stackView.backgroundColor = .systemOrange
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
+    }()
+    
+    private let homeLabelContainerView: UIStackView = {
+        let stackView: UIStackView = .init(frame: .zero)
+        stackView.axis = .horizontal
+        stackView.backgroundColor = .clear
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var homeLabel: UILabel = {
+        let label: UILabel = .init(frame: .zero)
+        label.text = "홈"
+        label.font = .systemFont(ofSize: 20)
+        label.textColor = .black
+        label.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapHomeLabel)))
+        label.isUserInteractionEnabled = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
     }()
     
     private let topVC: SideMenuTopViewController = .init()
@@ -47,13 +75,14 @@ final class SideMenuViewController: UIViewController {
         self.view.backgroundColor = .systemBackground
         
         configureBasicUI()
-        addTopVC()
+        setFirstSectionLayout()
         addPanGesture()
     }
     
     // MARK: - Helpers
     private func configureBasicUI() {
         view.addSubview(containerView)
+        view.addSubview(settingButton)
         
         containerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
         containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
@@ -66,12 +95,32 @@ final class SideMenuViewController: UIViewController {
         mainStackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
         mainStackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
         //mainStackView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor).isActive = true  -> 주석처리 안하면 UI깨짐
+        
+        settingButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        settingButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10).isActive = true
+        settingButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        settingButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
     }
     
-    private func addTopVC() {
+    private func setFirstSectionLayout() {
         addChild(topVC)
         mainStackView.addArrangedSubview(topVC.view)
         topVC.didMove(toParent: self)
+        
+        homeLabelContainerView.addArrangedSubview(homeLabel)
+        homeLabelContainerView.layoutMargins = .init(top: 0, left: 30, bottom: 0, right: 10)
+        homeLabelContainerView.isLayoutMarginsRelativeArrangement = true
+        
+        mainStackView.addArrangedSubview(homeLabelContainerView)
+        mainStackView.addArrangedSubview(getSeperatorLine())
+    }
+    
+    private func getSeperatorLine() -> UIView {
+        let line: UIView = .init(frame: .zero)
+        line.backgroundColor = .gray
+        line.translatesAutoresizingMaskIntoConstraints = false
+        line.heightAnchor.constraint(equalToConstant: 0.5).isActive = true
+        return line
     }
     
     private func addPanGesture() {
@@ -80,6 +129,14 @@ final class SideMenuViewController: UIViewController {
     }
 
     // MARK: - Actions
+    @objc private func didTapSettingButton() {
+        print("did Tap setting Button!")
+    }
+    
+    @objc private func didTapHomeLabel() {
+        print("did Tap HomeLabel!")
+    }
+    
     @objc private func hideMenu(_ sender: UIPanGestureRecognizer) {
         let viewTranslation = sender.translation(in: view)
  
