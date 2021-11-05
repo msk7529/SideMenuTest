89d106cee4d4e13d080388f0e94969f9c6dc9e8f
G.magpie <msk7529@naver.com>
2021-11-05 23:49:52 +0900

    stackView 테스트

--- SideMenuTest/SideMenuTest/Controller/HomeNextViewController.swift
+++ SideMenuTest/SideMenuTest/Controller/HomeNextViewController.swift
@@ -24,6 +24,43 @@ final class HomeNextViewController: UIViewController, SideMenuTranstionSuportabl
         return customSwitch
     }()
     
+    private let containerView: UIView = {
+        let view: UIView = .init(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let containerStackView: UIStackView = {
+        let view: UIStackView = .init(frame: .zero)
+        view.axis = .vertical
+        view.distribution = .equalSpacing
+        view.spacing = 10
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let imageContainerView: UIView = {
+        let view: UIView = .init(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let imageView: UIImageView = {
+        let imageView: UIImageView = .init(frame: .zero)
+        imageView.backgroundColor = .green
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let testLabel: UILabel = {
+        let label: UILabel = .init(frame: .zero)
+        label.text = "1231231123412341234"
+        label.font = .systemFont(ofSize: 14)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     internal lazy var transitionDelegate: SideMenuTransitionDelegate = .init()
     
     override func viewDidLoad() {
@@ -53,6 +90,34 @@ final class HomeNextViewController: UIViewController, SideMenuTranstionSuportabl
         customSwitch.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 30).isActive = true
         customSwitch.widthAnchor.constraint(equalToConstant: 130).isActive = true
         customSwitch.heightAnchor.constraint(equalToConstant: 35).isActive = true
+        
+        self.view.addSubview(containerView)
+        
+        containerView.topAnchor.constraint(equalTo: customSwitch.bottomAnchor, constant: 20).isActive = true
+        containerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10).isActive = true
+        containerView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10).isActive = true
+        containerView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        self.containerView.addSubview(containerStackView)
+        
+//        containerStackView.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
+        containerStackView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor).isActive = true
+//        containerStackView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor).isActive = true
+        containerStackView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+        
+        containerStackView.addArrangedSubview(imageContainerView)
+        containerStackView.addArrangedSubview(testLabel)
+
+        imageContainerView.addSubview(imageView)
+        
+        imageContainerView.topAnchor.constraint(equalTo: imageView.topAnchor).isActive = true
+        imageContainerView.centerXAnchor.constraint(equalTo: imageView.centerXAnchor).isActive = true
+        imageContainerView.heightAnchor.constraint(equalTo: imageView.heightAnchor).isActive = true
+                
+        imageView.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        //testLabel.setContentHuggingPriority(UILayoutPriority(999), for: .horizontal)
     }
     
     @objc private func didTapMenuButton() {
